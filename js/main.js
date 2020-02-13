256f1f03c53c225fbbf392f36a84bb0a682e034d
Ilya Evdokimov <devdokimov.ilya@yandex.ru>
2020-02-13 20:54:16 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function () {
 var s_items = $('.portfolio__item').length;
 for (let i = 0; i < s_items; i++) {
-    $('.slider__dotted').append('<div data-slide = '+i+' class="slider__dot"></div>');
+    if(i==0){
+        $('.slider__dotted').append('<div data-slide = '+i+' class="slider__dot active"></div>');
+    }
+    else{
+        $('.slider__dotted').append('<div data-slide = '+i+' class="slider__dot"></div>');
+    }
 }
     $(".slider").slick({
         autoplay: false,
@@ -9,6 +14,14 @@ for (let i = 0; i < s_items; i++) {
     });
 
     $('.slider__dot').on('click',function(){
+        $('.active').removeClass('active');
         slide = $(this).attr('data-slide');
+        $('.slider').slick('slickGoTo', slide);
+        $(this).toggleClass('active');
+    });
+
+    $(".slider").on('afterChange', function(event, slick, currentSlide){
+        $('.active').removeClass('active');
+        $('.slider__dot[data-slide='+currentSlide+']').toggleClass('active')
     });
 });
