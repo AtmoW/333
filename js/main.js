82abca664dcf86bb0b3d1ff73bb82e044a195af0
Ilya Evdokimov <devdokimov.ilya@yandex.ru>
2020-02-13 20:41:05 +0300

    not working dots but is about

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
-    
+var s_items = $('.portfolio__item').length;
+for (let i = 0; i < s_items; i++) {
+    $('.slider__dotted').append('<div data-slide = '+i+' class="slider__dot"></div>');
+}
+    $(".slider").slick({
+        autoplay: false,
+        arrows:false
+    });
+
+    $('.slider__dot').on('click',function(){
+        slide = $(this).attr('data-slide');
+    });
 });
